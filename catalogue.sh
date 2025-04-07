f308d89208e4b24b691c24bec57892838d546337
naveen <naveenkumar9707@gmail.com>
2025-04-07 14:05:09 -0500

    Catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,15 +1,27 @@
+echo -e "\e[36m>>>>>>>>> configuring and downloading nodejs <<<<<<<<\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
+echo -e "\e[36m>>>>>>>>> Adding a Application User <<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[36m>>>>>>>>> Creating a Application Directory<<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e[36m>>>>>>>>> Downloading Application Code<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+echo -e "\e[36m>>>>>>>>> Unzip Application Code<<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
+echo -e "\e[36m>>>>>>>>> Downloading nodejs dependencies <<<<<<<<\e[0m"
 npm install
-#cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[36m>>>>>>>>> copying service file to systemd <<<<<<<<\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[36m>>>>>>>>> Starting Catalogue service <<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
+echo -e "\e[36m>>>>>>>>> Copying Mongodb repo file <<<<<<<<\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>> Downloading Mongodb <<<<<<<<\e[0m"
 dnf install mongodb-org-shell -y
-#mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<\e[0m"
+mongo --host mongodb-dev.kruthikadevops.online </app/schema/catalogue.js
