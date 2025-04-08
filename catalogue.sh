cd30bc07bfc508782e78007478145fd4742e9b10
naveen <naveenkumar9707@gmail.com>
2025-04-08 12:43:46 -0500

    common.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,7 @@
-script_path=$(dirname $0)
+script=$(realpath $0)
+script_path=${dirname $script}
 echo ${script_path}
+exit
 
 source ${script_path}/common.sh
 echo -e "\e[36m>>>>>>>>> configuring and downloading nodejs <<<<<<<<\e[0m"
