effb61598f636c81e923762e4ca46d8cbbd72aab
naveen <naveenkumar9707@gmail.com>
2025-04-14 11:23:07 -0500

    mongo script fixes

--- mongo.sh
+++ mongo.sh
@@ -5,7 +5,7 @@ source ${script_path}/common.sh
 func_print_head "Configuring Mongodb repo"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 func_print_head "Installing Mongodb"
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$log_file
 func_print_head "Changing Mongodb configuration"
 sed -i -e "s|127.0.0.1|0.0.0.0|" /etc/mongod.conf
 func_print_head "Starting Mongodb"
