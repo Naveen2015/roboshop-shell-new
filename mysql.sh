123717e0395fe7a5b08f95b2c50d355801dac212
naveen <naveenkumar9707@gmail.com>
2025-04-14 12:05:42 -0500

    mysql script fixes

--- mysql.sh
+++ mysql.sh
@@ -8,11 +8,13 @@ then
 echo "Mysql password is missing"
 fi
 
-echo -e "\e[36m>>>>>>>>> Installing Mysql <<<<<<<<\e[0m"
-dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
-dnf install mysql-community-server -y
-echo -e "\e[36m>>>>>>>>> Staring Mysql <<<<<<<<\e[0m"
+func_print_head "Installing Mysql"
+dnf module disable mysql -y &>>$log_file
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+dnf install mysql-community-server -y &>>$log_file
+func_print_head "Staring Mysql"
 systemctl enable mysqld
-systemctl restart mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+systemctl restart mysqld &>>$log_file
+func_stat_check $?
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
+func_stat_check $?
