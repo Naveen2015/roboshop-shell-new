67064dbff8e9ac3cea27d34d7ee4d230ba655e9e
naveen <naveenkumar9707@gmail.com>
2025-04-14 11:47:09 -0500

    redis script fixes

--- redis.sh
+++ redis.sh
@@ -2,14 +2,13 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-func_print_head "configuring and downloading rabbitmq"
+func_print_head "configuring and downloading redis"
 dnf module disable redis -y &>>$log_file
 dnf module enable redis:6 -y &>>$log_file
 dnf install redis -y &>>$log_file
 func_stat_check $?
 func_print_head "changing the configuration"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf &>>$log_file
 func_print_head "Enabling redis"
 systemctl enable redis
 systemctl restart redis
