1e6e055d199ecbe28c4901feebf8d8d8528c1f41
naveenporeddy <naveenkumar9707@gmail.com>
2025-04-19 23:07:17 -0500

    issue checker

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ dnf module enable redis:6 -y &>>$log_file
 dnf install redis -y &>>$log_file
 func_stat_check $?
 func_print_head "changing the configuration"
-sudo sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf &>>$log_file
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf &>>$log_file
 func_stat_check $?
 func_print_head "Enabling redis"
 systemctl enable redis
